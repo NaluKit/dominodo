2a69e57a95f66d1409ca38d0ab925d50e41d55ba
Frank Hossfeld <frank.hossfeld@googlemail.com>
2019-05-14 12:28:01 +0200

    add webapp to domino-j2cl

--- dominodo/dominodo-j2cl/src/main/java/org/dominokit/samples/App.java
+++ dominodo/dominodo-j2cl/src/main/java/org/dominokit/samples/App.java
@@ -1,12 +1,18 @@
 package org.dominokit.samples;
 
-import com.google.gwt.core.client.EntryPoint;
+    import com.google.gwt.core.client.EntryPoint;
+    import org.gwtproject.safehtml.shared.annotations.GwtIncompatible;
 
 public class App implements EntryPoint {
 
-    public void onModuleLoad() {
-      DominoDo application = new DominoDo();
-      application.run();
-    }
+  public void onModuleLoad() {
+    DominoDo application = new DominoDo();
+    application.run();
+  }
 
+
+  @GwtIncompatible()
+  public void emptyPuppa() {
+    System.out.println("PIPPO IS SUPER STRONG");
+  }
 }
